703d8899f50e4f9754e9092c3c0504fb661c0f10
Bernardo Poggioni <hckranonymous2@hotmail.com>
2024-03-12 22:03:53 -0300

    Success Submit Status added

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,17 +1,23 @@
 <script setup>
 import {reactive} from 'vue'
 
-    let errorMessage = 'Please provide a valid email address'
+    let submitStatusMessage = 'Please provide a valid email address'
 
     const inputEmail = reactive({
     email: '',
-    error: false
+    error: false,
+    success: false
     })
 
     const inputErrorHandler = (err) => {
-    errorMessage = err
+    submitStatusMessage = err
     inputEmail.error = true
-    console.log(errorMessage)
+    console.log(submitStatusMessage)
+    }
+
+    const inputClear = () => {
+        submitStatusMessage = 'Email successfully sent'
+        inputEmail.email = ''
     }
 
     const checkInput = () => {
@@ -22,14 +28,21 @@ import {reactive} from 'vue'
         } else if (!inputEmail.email.includes('.com')){
             inputErrorHandler('Email needs .com')
         } else {
+            inputClear()
+            inputEmail.success = true
             inputEmail.error = false
         }
     }
 </script>
 <template>
     <form @submit.prevent class="page__form">
-      <input :class="[inputEmail.error?'page__form-email input_error':'page__form-email']" type="email" placeholder="Your email address..." v-model="inputEmail.email"/>      
-      <p v-if="inputEmail.error" class="page__form-error">{{ errorMessage }}</p>
+      <input type="email" placeholder="Your email address..." v-model="inputEmail.email" 
+      :class="[
+        inputEmail.error?'page__form-email input_error':'page__form-email', 
+        inputEmail.success?'page__form-email input_success':'page__form-email']" 
+        />      
+      <p v-if="inputEmail.error" class="page__form-error">{{ submitStatusMessage }}</p>
+      <p v-if="inputEmail.success" class="page__form-success">{{ submitStatusMessage }}</p>
       <button @:click="checkInput" class="page__form-btn" type="submit">Notify Me</button>
     </form>
 </template>
